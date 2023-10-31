b6418c076d2d83b79ce5a953481967564f82f5ff
DiegoCuriel <a01640372@itesm.mx>
2023-10-30 22:04:42 -0600

    Login - inputs email y pass

--- app/login.js
+++ app/login.js
@@ -1,12 +1,44 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, TextInput } from 'react-native'
 import React from 'react'
+import { StatusBar } from 'expo-status-bar'
 
 const login = () => {
   return (
-    <View>
-      <Text>ea!</Text>
+    <View style={styles.container}>
+      <Text style={styles.titulo}>Login</Text>
+      <Text style={styles.subTitulo}>¡Bienvenido de vuelta!</Text>
+      <TextInput style={styles.input} placeholder="ejemplo@gmail.com" />
+      <TextInput style={styles.input} placeholder="contraseña" />
+      <StatusBar style="auto" />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f1f1f1",
+    alignItems: "center",
+    justifyContent:'center',
+  },
+  titulo: {
+    fontSize: 80,
+    color: '#34434D',
+    fontWeight: "bold",
+  },
+  subTitulo: {
+    fontSize: 20,
+    color: "gray",
+  },
+  input: {
+    padding: 10,
+    paddingStart: 30,
+    width: '80%',
+    height: 50,
+    marginTop: 20,
+    borderRadius: 30,
+    backgroundColor: '#fff',
+  },
+})
 
 export default login

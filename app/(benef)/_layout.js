202bd987f88838f3e3c1a75a1a3d053a6e3a9788
DiegoCuriel <a01640372@itesm.mx>
2023-10-31 01:05:31 -0600

    benef - navegacion, textos

--- app/(benef)/_layout.js
+++ app/(benef)/_layout.js
@@ -5,7 +5,16 @@ import { Tabs } from 'expo-router'
 export default function _layout() {
   return (
     <Tabs>
-        
+        <Tabs.Screen name="noticiasBenef" options={{
+            headerTitle: "Noticias",
+            headerTitleAlign: "center",
+            tabBarLabel: "Noticias"
+        }}/>
+        <Tabs.Screen name="perfilBenef" options={{
+            headerTitle: "Perfil",
+            headerTitleAlign: "center",
+            tabBarLabel: "Perfil"
+        }}/>
     </Tabs>
   )
 }

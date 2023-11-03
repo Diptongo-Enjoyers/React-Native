0eb6e6499869bd3d24aad13352ea2ee4cd080615
DiegoCuriel <a01640372@itesm.mx>
2023-11-02 19:18:47 -0600

    NoticiaAdmin - modal agregado

--- app/(admin)/noticiasAdmin.js
+++ app/(admin)/noticiasAdmin.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Image, StyleSheet, FlatList, Pressable } from 'react-native';
+import { View, Text, Image, StyleSheet, FlatList, Pressable, Button, Modal, TextInput, TouchableOpacity } from 'react-native';
 import { Link, useRouter } from 'expo-router';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import BottomTabBarAdmin from '../../Components/BottomTabBarAdmin';
@@ -8,6 +8,11 @@ export default function noticiasAdmin() {
     const [noticias, setNoticias] = useState([]);
     const selectedTab = 'noticiasAdmin';
     const router = useRouter();
+
+    const [modalVisible, setModalVisible] = useState(false);
+    const [titulo, setTitulo] = useState('');
+    const [descripcion, setDescripcion] = useState('');
+    const [imagen, setImagen] = useState('');
 
     useEffect(() => {
         const fetchNoticias = async () => {
@@ -54,6 +59,58 @@ export default function noticiasAdmin() {
 
     return (
         <View style={styles.container}>
+
+            <Button title="Abrir Modal" onPress={() => setModalVisible(true)} />
+            {/* Modal */}
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                    setModalVisible(!modalVisible);
+                }}>
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <TextInput
+                            placeholder="Título"
+                            value={titulo}
+                            onChangeText={setTitulo}
+                            style={styles.input}
+                        />
+                        <TextInput
+                            placeholder="Descripción"
+                            value={descripcion}
+                            onChangeText={setDescripcion}
+                            style={styles.input}
+                        />
+                        <TextInput
+                            placeholder="URL de la Imagen"
+                            value={imagen}
+                            onChangeText={setImagen}
+                            style={styles.input}
+                        />
+                        <TouchableOpacity
+                            style={styles.button}
+                            onPress={() => {
+                                
+                                setModalVisible(false);
+                            }}>
+                            <Text>Confirmar</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            style={styles.button}
+                            onPress={() => {
+                                setTitulo('');
+                                setDescripcion('');
+                                setImagen('');
+                                setModalVisible(false);
+                            }}>
+                            <Text>Cancelar</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </Modal>
+            
             <FlatList
                 data={noticias}
                 renderItem={renderItem}
@@ -91,5 +148,40 @@ const styles = StyleSheet.create({
     descripcion: {
         fontSize: 16,
         marginBottom: 8,
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 22,
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: "white",
+        borderRadius: 20,
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#ccc',
+        width: 200,
+        padding: 10,
+        marginBottom: 10
+    },
+    button: {
+        backgroundColor: "#2196F3",
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2,
+        margin: 5
     },
 });

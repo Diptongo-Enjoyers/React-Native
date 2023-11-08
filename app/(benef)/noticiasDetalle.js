b2403137603add82cc7adb889e4bd7866c8a60c7
HugoGoHe <A01640856@itesm.mx>
2023-11-08 14:18:40 -0600

    Arrow button news detailsView

--- app/(benef)/noticiasDetalle.js
+++ app/(benef)/noticiasDetalle.js
@@ -1,5 +1,8 @@
-import { View, Text, Image, StyleSheet, Button } from "react-native";
+import React from 'react';
+import { View, Text, Image, StyleSheet, Button, TouchableOpacity } from "react-native";
 import { useLocalSearchParams, useRouter } from "expo-router";
+import Icon from 'react-native-vector-icons/Ionicons'; // Choose appropriate icon set
+
 
 const SingleHouseScreen = () => {
   const { titulo, descripcion, imagen } = useLocalSearchParams();
@@ -7,7 +10,9 @@ const SingleHouseScreen = () => {
 
   return (
     <View style={styles.container}>
-      <Button title="Volver" onPress={back} />
+      <TouchableOpacity onPress={back} style={styles.backButton}>
+      <Icon name="arrow-back" size={35} color="#0093F2" />
+    </TouchableOpacity>
       <Image source={{ uri: imagen }} style={styles.imagen} />
       <Text style={styles.titulo}>{titulo}</Text>
       <Text style={styles.descripcion}>{descripcion}</Text>
@@ -25,7 +30,7 @@ const styles = StyleSheet.create({
     height: 400,
     borderRadius: 8,
     marginBottom: 40,
-    marginTop: 20,
+    marginTop: 50,
   },
   titulo: {
     fontSize: 20,
@@ -35,6 +40,14 @@ const styles = StyleSheet.create({
   descripcion: {
     fontSize: 16,
   },
+  backButton: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+    position: 'absolute', // Position absolutely within the parent view
+    top: 10,             // Adjust top and left as needed for your layout
+    left: 10,            // This provides some space from the corners
+  },  
 });
 
 export default SingleHouseScreen;

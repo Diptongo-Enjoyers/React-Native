4232df48f5d7fd3a54a5b93f8bc058e994ed2f1e
DiegoCuriel <a01640372@itesm.mx>
2023-11-21 14:20:01 -0600

    EditUserAdmin - PATCH de clearance

--- app/(admin)/editarUserAdmin.js
+++ app/(admin)/editarUserAdmin.js
@@ -22,6 +22,8 @@ import {
   ScrollView,
   Box,
   KeyboardAvoidingView,
+  Select,
+  CheckIcon
 } from "native-base";
 
 const clearance = (clearance) => {
@@ -42,6 +44,7 @@ const clearance = (clearance) => {
 export default function editarPerfilDonador() {
   const router = useRouter();
   const [user, setUser] = useState({});
+  const [selectedRole, setSelectedRole] = useState(user.clearance);
 
   useEffect(() => {
     const fetchUserInformation = async () => {
@@ -66,6 +69,7 @@ export default function editarPerfilDonador() {
 
         const data = await response.json();
         setUser(data); // Actualiza el estado con la información del usuario
+        setSelectedRole(data.clearance.toString());
       } catch (error) {
         console.error("Error al obtener la información del usuario:", error);
       }
@@ -74,15 +78,29 @@ export default function editarPerfilDonador() {
     fetchUserInformation();
   }, []);
 
+  const handleRoleChange = (newValue) => {
+    setSelectedRole(newValue);
+    setUser((prevUser) => ({
+      ...prevUser,
+      clearance: parseInt(newValue, 10), // Asegúrate de que clearance sea un número si así se requiere.
+    }));
+  };
+
   const updateUserInformation = async () => {
     const token = await AsyncStorage.getItem("userToken");
-    const userId = await AsyncStorage.getItem("selectedItemId"); // Obtiene el ID del usuario
-
+    const userId = await AsyncStorage.getItem("selectedItemId");
+  
     if (!userId || !token) {
       console.error("No se encontró el ID del usuario o el token.");
       return;
     }
-
+  
+    // Asegúrate de convertir el valor de selectedRole a número, si clearance en la base de datos es numérico.
+    const updatedUser = {
+      ...user,
+      clearance: parseInt(selectedRole, 10), // Convierte el valor de selectedRole a número
+    };
+  
     try {
       const response = await fetch(
         `https://api-three-kappa-45.vercel.app/users/${userId}`, // Usa el ID del usuario en la URL
@@ -92,21 +110,22 @@ export default function editarPerfilDonador() {
             Authorization: `Bearer ${token}`,
             "Content-Type": "application/json",
           },
-          body: JSON.stringify(user),
+          body: JSON.stringify(updatedUser), // Envía el usuario actualizado
         }
       );
-
+  
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
-
+  
       const data = await response.json();
       console.log(data);
+      // Puedes querer manejar la respuesta de alguna manera, por ejemplo, informar al usuario que la actualización fue exitosa.
     } catch (error) {
       console.error("Error al actualizar la información del usuario:", error);
     }
   };
-
+  
   return (
     <NativeBaseProvider>
       <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} flex={1}>
@@ -117,158 +136,160 @@ export default function editarPerfilDonador() {
                 Editar perfil
               </Heading>
               <Divider my={2} />
-              <ScrollView>
-                <FormControl>
-                  <VStack>
-                    <FormControl.Label>
-                      URL de la imagen de perfil
-                    </FormControl.Label>
-                    <Input
-                      w={"100%"}
-                      variant="rounded"
-                      InputLeftElement={
-                        <Icon
-                          as={<MaterialIcons name="link" />}
-                          size={5}
-                          ml="2"
-                        />
-                      }
-                      value={user.profilePictureURL}
-                      onChangeText={(text) =>
-                        setUser({ ...user, profilePictureURL: text })
-                      }
-                    />
-                  </VStack>
-                </FormControl>
-                <FormControl pt={6}>
-                  <VStack>
-                    <FormControl.Label>Nombre</FormControl.Label>
-                    <Input
-                      variant="rounded"
-                      InputLeftElement={
-                        <Icon
-                          as={<MaterialIcons name="person" />}
-                          size={5}
-                          ml="2"
-                        />
-                      }
-                      value={user.name}
-                      onChangeText={(text) => setUser({ ...user, name: text })}
-                    />
-                  </VStack>
-                </FormControl>
-                <FormControl>
-                  <VStack>
-                    <FormControl.Label>Usuario</FormControl.Label>
-                    <Input
-                      variant="rounded"
-                      InputLeftElement={
-                        <Icon
-                          as={<MaterialIcons name="alternate-email" />}
-                          size={5}
-                          ml="2"
-                        />
-                      }
-                      value={user.username}
-                      onChangeText={(text) =>
-                        setUser({ ...user, username: text })
-                      }
-                    />
-                  </VStack>
-                </FormControl>
-                <FormControl pt={6}>
-                  <VStack>
-                    <FormControl.Label>Autorización</FormControl.Label>
-                    <Input
-                      variant="rounded"
-                      InputLeftElement={
-                        <Icon
-                          as={<MaterialIcons name="verified-user" />}
-                          size={5}
-                          ml="2"
-                        />
-                      }
-                      value={clearance(user.clearance)}
-                      isReadOnly={true}
-                      isDisabled={true}
-                    />
-                  </VStack>
-                </FormControl>
-                <FormControl pt={6}>
-                  <VStack>
-                    <FormControl.Label>Teléfono</FormControl.Label>
-                    <Input
-                      variant="rounded"
-                      InputLeftElement={
-                        <Icon
-                          as={<MaterialIcons name="phone" />}
-                          size={5}
-                          ml="2"
-                        />
-                      }
-                      value={user.phone?.toString()}
-                      onChangeText={(text) => setUser({ ...user, phone: text })}
-                    />
-                  </VStack>
-                </FormControl>
-                <FormControl>
-                  <VStack>
-                    <FormControl.Label>Correo electrónico</FormControl.Label>
-                    <Input
-                      variant="rounded"
-                      InputLeftElement={
-                        <Icon
-                          as={<MaterialIcons name="email" />}
-                          size={5}
-                          ml="2"
-                        />
-                      }
-                      value={user.email}
-                      onChangeText={(text) => setUser({ ...user, email: text })}
-                    />
-                  </VStack>
-                </FormControl>
-                <FormControl>
-                  <VStack>
-                    <FormControl.Label>Dirección</FormControl.Label>
-                    <Input
-                      variant="rounded"
-                      InputLeftElement={
-                        <Icon
-                          as={<MaterialIcons name="location-on" />}
-                          size={5}
-                          ml="2"
-                        />
-                      }
-                      value={user.address}
-                      onChangeText={(text) =>
-                        setUser({ ...user, address: text })
-                      }
-                    />
-                  </VStack>
-                </FormControl>
-                <HStack space={2} alignItems={"flex-end"} pt={6}>
-                  <Button
-                    variant="outline"
-                    colorScheme="info"
-                    width={"50%"}
-                    onPress={() => router.replace("/usuariosAdmin")}
-                  >
-                    Cancelar
-                  </Button>
-                  <Button
-                    variant="solid"
-                    colorScheme="info"
-                    width={"50%"}
-                    onPress={() => {
-                      updateUserInformation();
-                      router.replace("/usuariosAdmin");
+              <FormControl>
+                <VStack>
+                  <FormControl.Label>
+                    URL de la imagen de perfil
+                  </FormControl.Label>
+                  <Input
+                    w={"100%"}
+                    variant="rounded"
+                    InputLeftElement={
+                      <Icon
+                        as={<MaterialIcons name="link" />}
+                        size={5}
+                        ml="2"
+                      />
+                    }
+                    value={user.profilePictureURL}
+                    onChangeText={(text) =>
+                      setUser({ ...user, profilePictureURL: text })
+                    }
+                  />
+                </VStack>
+              </FormControl>
+              <FormControl pt={6}>
+                <VStack>
+                  <FormControl.Label>Nombre</FormControl.Label>
+                  <Input
+                    variant="rounded"
+                    InputLeftElement={
+                      <Icon
+                        as={<MaterialIcons name="person" />}
+                        size={5}
+                        ml="2"
+                      />
+                    }
+                    value={user.name}
+                    onChangeText={(text) => setUser({ ...user, name: text })}
+                  />
+                </VStack>
+              </FormControl>
+              <FormControl>
+                <VStack>
+                  <FormControl.Label>Usuario</FormControl.Label>
+                  <Input
+                    variant="rounded"
+                    InputLeftElement={
+                      <Icon
+                        as={<MaterialIcons name="alternate-email" />}
+                        size={5}
+                        ml="2"
+                      />
+                    }
+                    value={user.username}
+                    onChangeText={(text) =>
+                      setUser({ ...user, username: text })
+                    }
+                  />
+                </VStack>
+              </FormControl>
+              <FormControl pt={6}>
+                <VStack>
+                  <FormControl.Label>Autorización</FormControl.Label>
+                  <Select
+                    selectedValue={selectedRole}
+                    minWidth="200"
+                    accessibilityLabel="Elige un rol"
+                    placeholder="Elige un rol"
+                    _selectedItem={{
+                      bg: "teal.600",
+                      endIcon: <CheckIcon size="5" />,
                     }}
+                    mt={1}
+                    onValueChange={handleRoleChange} // Usa el nuevo manejador aquí.
                   >
-                    Guardar
-                  </Button>
-                </HStack>
-              </ScrollView>
+                    <Select.Item label="Admin" value="0" />
+                    <Select.Item label="Empleado" value="1" />
+                    <Select.Item label="Donador" value="2" />
+                  </Select>
+                </VStack>
+              </FormControl>
+
+              <FormControl pt={6}>
+                <VStack>
+                  <FormControl.Label>Teléfono</FormControl.Label>
+                  <Input
+                    variant="rounded"
+                    InputLeftElement={
+                      <Icon
+                        as={<MaterialIcons name="phone" />}
+                        size={5}
+                        ml="2"
+                      />
+                    }
+                    value={user.phone?.toString()}
+                    onChangeText={(text) => setUser({ ...user, phone: text })}
+                  />
+                </VStack>
+              </FormControl>
+              <FormControl>
+                <VStack>
+                  <FormControl.Label>Correo electrónico</FormControl.Label>
+                  <Input
+                    variant="rounded"
+                    InputLeftElement={
+                      <Icon
+                        as={<MaterialIcons name="email" />}
+                        size={5}
+                        ml="2"
+                      />
+                    }
+                    value={user.email}
+                    onChangeText={(text) => setUser({ ...user, email: text })}
+                  />
+                </VStack>
+              </FormControl>
+              <FormControl>
+                <VStack>
+                  <FormControl.Label>Dirección</FormControl.Label>
+                  <Input
+                    variant="rounded"
+                    InputLeftElement={
+                      <Icon
+                        as={<MaterialIcons name="location-on" />}
+                        size={5}
+                        ml="2"
+                      />
+                    }
+                    value={user.address}
+                    onChangeText={(text) =>
+                      setUser({ ...user, address: text })
+                    }
+                  />
+                </VStack>
+              </FormControl>
+              <HStack space={2} alignItems={"flex-end"} pt={6}>
+                <Button
+                  variant="outline"
+                  colorScheme="info"
+                  width={"50%"}
+                  onPress={() => router.replace("/usuariosAdmin")}
+                >
+                  Cancelar
+                </Button>
+                <Button
+                  variant="solid"
+                  colorScheme="info"
+                  width={"50%"}
+                  onPress={() => {
+                    updateUserInformation();
+                    router.replace("/usuariosAdmin");
+                  }}
+                >
+                  Guardar
+                </Button>
+              </HStack>
             </VStack>
           </View>
         </TouchableWithoutFeedback>
